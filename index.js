549d71353e81644bbe315f6f17d3db148d710b22
uurtech <developer.ugur@gmail.com>
2020-02-17 12:16:37 +0300

    u

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const mammoth = require("mammoth");
-const express = require('express');
-const fileUpload = require('express-fileupload');
+const mammoth       = require("mammoth");
+const express       = require('express');
+const fileUpload    = require('express-fileupload');
+const md5           = require('md5');
+
 const app = express();
 
 // default options
@@ -13,7 +15,22 @@ app.use(fileUpload({
 app.get('/', function (req, res) {
     res.send("hello")
 })
+app.use(function(req,res,next){
+    let token = req.query.token || req.body.token || req.params.token || "";
+    if(token === ""){
+        res.send(403)
+        return;
+    }
+    let date = new Date();
+    let hash = md5(date.getUTCHours()) + md5(date.getUTCDate);
+    if (token !== hash){
+        res.send(403);
+        return;
+    }
+    next();
+})
 app.post('/upload', function (req, res) {
+
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
     }
